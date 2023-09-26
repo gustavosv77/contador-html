23d667e621ee1ac7e75f9c18e8fc1c281e73cec1
gustavosv77 <gustavosv.senai77@gmail.com>
2023-09-26 20:47:22 -0300

    feat: repair function aumentar

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 const h2 = document.querySelector("h2");
 
 function aumentar() {
-    // h2.innerText = h2.innerText + 1
+    // h2.innerText = +h2.innerTex + 1
+    // h2.innerText = parseInt(h2) + 1
     h2.innerText ++
 }
 
